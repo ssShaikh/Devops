35b74563ab23edfb338227a8f7ca05d9dc72ff2c
ssShaikh <48612979+ssShaikh@users.noreply.github.com>
2022-07-06 20:54:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "cidr_block" {
+
+  default = "0.0.0.0/0"
+}
+
+variable "vpc_id" {
+
+  default = "vpc-0880e783c03b0bd91"
+}
+
+variable "key_name" {
+
+  default = "ubuntu_instance"
+}
+
+variable "region" {
+
+  default = "us-east-2"
+}
+
+variable "access_key" {
+
+
+}
+
+variable "secret_key" {
+
+
+}
+
+
+
+variable "instance_type" {
+
+  default = "t2.micro"
+}
+
